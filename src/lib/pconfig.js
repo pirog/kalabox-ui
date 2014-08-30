8e8aa68e1309b0b22e04e718c9ef9b719d5acb26
Mike Milano <coder1@gmail.com>
2014-08-30 14:42:49 -0700

    replaced reduce with getIndex to simplify things

--- src/lib/pconfig.js
+++ src/lib/pconfig.js
@@ -4,21 +4,18 @@ var _ = require('lodash'),
   platform = require('os').platform(),
   raw_config = require('../../config/platform.json');
 
-// reduce callback to get platform index
-var getIndex = function(result, val, key) {
-  if (result !== false) {
-    return result;
-  }
-  return val == platform ? key : false;
+// findIndex callback to get platform
+var getIndex = function(val) {
+  return val === platform;
 };
 
-// map callback to get a specific platform config.
+// map callback to set a specific platform config.
 var getConfig = function(options, key, config) {
   return [key, key == 'index' ? config.index : options[config.index]];
 };
 
 // set index
-raw_config.index = _.reduce(raw_config.platform, getIndex, false);
+raw_config.index = _.findIndex(raw_config.platform, getIndex);
 
 // export a processed config
 module.exports = _.object(_.map(raw_config, getConfig));
