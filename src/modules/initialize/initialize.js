9e9ccdac94836bea8d8027e50cc76dcb3a5248e5
Mike Milano <coder1@gmail.com>
2014-08-31 13:51:18 -0700

    double quotes to single

--- src/modules/initialize/initialize.js
+++ src/modules/initialize/initialize.js
@@ -13,7 +13,7 @@ angular.module('kalabox.initialize', [])
 
       var gui = require('nw.gui');
       var mb = new gui.Menu({type: 'menubar'});
-      mb.createMacBuiltin("Kalabox", {hideEdit: true, hideWindow: true});
+      mb.createMacBuiltin('Kalabox', {hideEdit: true, hideWindow: true});
       gui.Window.get().menu = mb;
 
       // Best practices is to manage our data in a scope object
