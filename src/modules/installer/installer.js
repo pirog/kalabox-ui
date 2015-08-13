34d32d0673fb25e30869e9dee5d9e1ebdd9768e3
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-08-13 14:47:29 -0700

    Added provisioning code to installer module.

--- src/modules/installer/installer.js
+++ src/modules/installer/installer.js
@@ -1,51 +1,76 @@
 'use strict';
 
 angular.module('kalabox.installer', [
-  'ngRoute'
+  'ngRoute',
+  'kalabox.nodewrappers'
 ])
 .config(function ($routeProvider) {
   $routeProvider.when('/installer', {
     templateUrl: 'modules/installer/installer.html',
     controller: 'InstallerCtrl'
   });
-  $routeProvider.when('/installer/download', {
-    templateUrl: 'modules/installer/installer.html',
-    controller: 'InstallerDownloadCtrl'
-  });
-  $routeProvider.when('/installer/vbox', {
-    templateUrl: 'modules/installer/installer.html',
-    controller: 'InstallerVBoxCtrl'
-  });
-  $routeProvider.when('/installer/docker', {
-    templateUrl: 'modules/installer/installer.html',
-    controller: 'InstallerDockerCtrl'
-  });
 })
-.controller('InstallerCtrl', ['$scope', function ($scope) {
+.controller('InstallerCtrl',
+['$scope', '$q', '$location', 'kbox',
+function ($scope, $q, $location, kbox) {
+
+  // Init ui model.
   $scope.ui = {
-    title: 'Checking Prerequisites',
-    detail: 'Testing Firewall',
-    stepProgress: 5
-  };
-}])
-.controller('InstallerDownloadCtrl', ['$scope', function ($scope) {
-  $scope.ui = {
-    title: 'Downloading Files',
+    title: '',
     detail: '',
-    stepProgress: 5
+    stepProgress: 0
   };
-}])
-.controller('InstallerVBoxCtrl', ['$scope', function ($scope) {
-  $scope.ui = {
-    title: 'Installing VirtualBox',
-    detail: '',
-    stepProgress: 5
+
+  // Create promise;
+  var deferred = $q.defer();
+
+  // Build initial state.
+  var state = {
+    disksize: 5 * 1000,
+    password: true,
+    nonInteractive: true,
+    adminCommands: [],
+    config: kbox.core.deps.get('config'),
+    downloads: [],
+    containers: [],
+    log: kbox.core.log,
+    stepIndex: 0,
+    status: true
   };
-}])
-.controller('InstallerDockerCtrl', function ($scope) {
-  $scope.ui = {
-    title: 'Installing Docker',
-    detail: '',
-    stepProgress: 5
-  };
-});
+
+  // Event called before a step runs.
+  kbox.install.events.on('pre-step', function(step) {
+    $scope.ui.title = step.description;
+    $scope.ui.detail = step.name;
+  });
+
+  // Event called after a step is finished running.
+  kbox.install.events.on('post-step', function() {
+    state.stepIndex += 1;
+    $scope.ui.stepProgress = state.stepIndex * (100 / state.stepCount);
+    $scope.$apply();
+  });
+
+  // OH NO!!!! an error has happened. :(
+  kbox.install.events.on('error', function(err) {
+    console.log(err.message + '\n' + err.stack);
+    $scope.ui.title = 'ERROR: ' + err.message;
+    deferred.reject(err);
+    $scope.$apply();
+  });
+
+  // Event called after provisioning has finished.
+  kbox.install.events.on('end', function() {
+    $scope.ui.title = 'Done installing!';
+    $location.path('/dashboard');
+    deferred.resolve();
+    $scope.$apply();
+  });
+
+  // Run provisioning.
+  kbox.install.run(state);
+
+  // Return promise.
+  return deferred.promise;
+
+}]);
