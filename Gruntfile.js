310838025a979252167a1314d6444c6c1fcbf517
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-08-16 12:58:27 -0700

    Fixing testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,7 +139,7 @@ module.exports = function(grunt) {
         command: 'cd ./generated && npm install --production --ignore-scripts'
       },
       foo: {
-        command: 'pwd && ls -l ./test/support && ls -l ./test/support/chromedriver'
+        command: 'pwd && ls -l && ls -l ./test/support && ls -l ./test/support/chromedriver'
       }
     },
     jshint: {
