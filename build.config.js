959e80962a5252b7911df8d26e8bb2333166c1cc
Alec Reynolds <alec@kalamuna.com>
2016-01-13 16:15:16 -0800

    kalabox/kalabox-ui#119: Non-working draft of integrating ngbp-style gruntfile modifications.

---
+++ build.config.js
@@ -0,0 +1,82 @@
+/**
+ * This file/module contains all configuration for the build process.
+ */
+module.exports = {
+  /**
+   * The `build_dir` folder is where our projects are compiled during
+   * development and the `compile_dir` folder is where our app resides once it's
+   * completely built.
+   */
+  build_dir: 'build',
+  compile_dir: 'generated',
+
+  /**
+   * This is a collection of file patterns that refer to our app code (the
+   * stuff in `src/`). These file paths are used in the configuration of
+   * build tasks. `js` is all project javascript, less tests. `ctpl` contains
+   * our reusable components' (`src/common`) template HTML files, while
+   * `atpl` contains the same, but for our app's code. `html` is just our
+   * main HTML file, `less` is our main stylesheet, and `unit` contains our
+   * app's unit tests.
+   */
+  app_files: {
+    js: [ 'src/modules/**/*.js', '!src/modules/**/*.spec.js' ],
+    jsunit: [ 'src/**/*.spec.js' ],
+
+    atpl: [ 'src/**/*.html' ],
+    ctpl: [ 'src/**/*.html' ],
+
+    html: [ 'src/index.html' ],
+    less: 'src/less/main.less'
+  },
+
+  /**
+   * This is a collection of files used during testing only.
+   */
+  test_files: {
+    js: [
+      'vendor/angular-mocks/angular-mocks.js'
+    ]
+  },
+
+  /**
+   * This is the same as `app_files`, except it contains patterns that
+   * reference vendor code (`vendor/`) that we need to place into the build
+   * process somewhere. While the `app_files` property ensures all
+   * standardized files are collected for compilation, it is the user's job
+   * to ensure non-standardized (i.e. vendor-related) files are handled
+   * appropriately in `vendor_files.js`.
+   *
+   * The `vendor_files.js` property holds files to be automatically
+   * concatenated and minified with our project source files.
+   *
+   * The `vendor_files.css` property holds any CSS files to be automatically
+   * included in our app.
+   *
+   * The `vendor_files.assets` property holds any assets to be copied along
+   * with our app's assets. This structure is flattened, so it is not
+   * recommended that you use wildcards.
+   */
+  vendor_files: {
+    js: [
+      'src/lib/vendor/bluebird/js/browser/bluebird.js',
+      'src/lib/vendor/jquery/dist/jquery.js',
+      'src/lib/vendor/d3/d3.js',
+      'src/lib/vendor/angular-ui-utils/modules/route/route.js',
+      'src/lib/vendor/angular/angular.js',
+      'src/lib/vendor/angular-ui-router/release/angular-ui-router.min.js',
+      'src/lib/vendor/angular-bootstrap/ui-bootstrap.min.js',
+      'src/lib/vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
+      'src/lib/vendor/angular-bluebird-promises/dist/angular-bluebird-promises.js',
+      'src/lib/vendor/jasny-bootstrap/dist/js/jasny-bootstrap.min.js'
+    ],
+    css: [
+      'src/lib/vendor/bootstrap/dist/css/bootstrap.min.css',
+      'src/lib/vendor/font-awesome/css/font-awesome.min.css',
+      'src/lib/vendor/jasny-bootstrap/dist/css/jasny-bootstrap.min.css'
+    ],
+    assets: [
+    ]
+  },
+};
+
--- src/modules/dashboard/test/e2e/main.spec.js
+++
@@ -1,9 +0,0 @@
-describe("dashboard module tests", function () {
-  describe("dashboard", function () {
-    it("should display the message set in scope", function () {
-      browser.get('/dashboard');
-      var message = element(by.binding('messageText'));
-      expect(message.getText()).toBe('Kalabox dashboard module.');
-    });
-  });
-});
