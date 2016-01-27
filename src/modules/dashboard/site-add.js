8cdb86c510606a045469ac0eb91f53f0312fc9aa
Alec Reynolds <alec@kalamuna.com>
2016-01-26 22:17:36 -0800

    kalabox/kalabox#873: Insert forgotten semi-colons.

--- src/modules/dashboard/site-add.js
+++ src/modules/dashboard/site-add.js
@@ -68,5 +68,4 @@ angular.module('kalabox.dashboard')
       });
     };
   });
-
-})
+});
--- src/modules/dashboard/site-pull.js
+++ src/modules/dashboard/site-pull.js
@@ -81,4 +81,4 @@ angular.module('kalabox.dashboard')
       $modalInstance.dismiss('cancel');
     };
   });
-})
+});
