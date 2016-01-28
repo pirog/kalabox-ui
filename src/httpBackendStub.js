2bec25a8aa3b2e2ae99a1057ff494d9d7aff1e89
Alec Reynolds <alec@kalamuna.com>
2016-01-27 18:13:42 -0800

    Add some more stubbed backend samples.

--- src/httpBackendStub.js
+++ src/httpBackendStub.js
@@ -12,17 +12,28 @@
     $delegate.sites = function() {
       return Promise.resolve([
           {
-            name: "Some App",
-            image: "blank",
+            name: "cvt",
+            image: "https://s3.amazonaws.com/pantheon-screenshots/183a4fd4-9ab4-4077-881d-e8468881e874_dev.png",
             provider: 'pantheon',
+            framework: 'drupal',
             trash: function() {
               return true;
             }
           },
           {
-            name: "Other App",
-            image: "blank",
+            name: "csi-auth-proxy",
+            image: "https://s3.amazonaws.com/pantheon-screenshots/d043b070-5f68-43c6-a7a7-0be09c1a9dcd_dev.png",
             provider: 'pantheon',
+            framework: 'drupal',
+            trash: function() {
+              return true;
+            }
+          },
+          {
+            name: "The Clayman Institute for Gender Research",
+            image: "https://s3.amazonaws.com/pantheon-screenshots/cf21d683-36f4-4caa-a2b5-cc66524bbb4d_dev.png",
+            provider: 'pantheon',
+            framework: 'drupal',
             trash: function() {
               return true;
             }
