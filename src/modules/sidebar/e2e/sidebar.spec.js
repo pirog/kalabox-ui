d083cf74b2e8c16054232b0ffb8b0864dbfeb2de
Alec Reynolds <alec@kalamuna.com>
2016-04-20 13:40:52 -0700

    kalabox/kalabox#1258: Working test for adding pantheon provider.

--- src/modules/sidebar/e2e/sidebar.spec.js
+++ src/modules/sidebar/e2e/sidebar.spec.js
@@ -1,22 +1,35 @@
 'use strict';
 
+var EC = protractor.ExpectedConditions;
+
 describe('sidebar module tests', function() {
   beforeEach(function() {
     browser.get('/dashboard');
     var addSite = $('div.site.add a');
-    addSite.click();
+    var isClickable = EC.elementToBeClickable(addSite);
+    browser.wait(isClickable, 5000)
+    .then(function() {
+      addSite.click();
+    });
   });
   afterEach(function() {
   });
   it('allow Pantheon sign-in', function() {
     $('ul.providers-next a').click().then(function() {
-      return expect($('h4').getText())
-      .toBe('AUTHENTICATE WITH PANTHEON');
+      var authPage = $('div.pantheon-authorization');
+      var authPageLoaded = EC.presenceOf(authPage);
+      return browser.wait(authPageLoaded, 5000);
+    })
+    .then(function() {
+      return expect($('h4').getText()).toBe('AUTHENTICATE WITH PANTHEON');
     }).then(function() {
       $('input#authEmail').sendKeys(process.env.PANTHEON_USER);
       // Need to figure out secret key sending.
       $('input#authPassword').sendKeys(process.env.PANTHEON_PASSWORD);
       return $('button.btn-primary').click();
+    }).then(function() {
+      var loaderPresent = EC.presenceOf($('div.loader'));
+      return browser.wait(loaderPresent, 5000);
     }).then(function() {
       return expect($('.loader h4').getText())
       .toBe('AUTHENTICATING');
