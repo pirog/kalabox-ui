4fff11ef4a8ef5918a7de5cdff23143862289232
Alec Reynolds <alec@kalamuna.com>
2016-05-26 11:19:47 -0700

    kalabox/kalabox#1274: Fix issue with initialize test.

--- src/modules/initialize/e2e/initialize.spec.js
+++ src/modules/initialize/e2e/initialize.spec.js
@@ -1,11 +1,16 @@
 'use strict';
 
+var EC = protractor.ExpectedConditions;
+
 describe('initialize module tests', function() {
   afterEach(function() {
   });
   it('should have initialize text', function() {
     browser.get('/initialize');
     var message = $('h1');
-    expect(message.getText()).toBe('Starting the Kalabox engine...');
+    var messagePresent = EC.presenceOf(message);
+    return browser.wait(messagePresent).then(function() {
+      expect(message.getText()).toBe('Starting the Kalabox engine...');
+    });
   });
 });
